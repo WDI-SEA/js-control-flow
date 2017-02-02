0983a818e9225dc2a1aa1aaae204481bae5a4ec9
challenge4better@gmail.com <lemonissweet@Meng-Lis-MacBook-Air.local>
2017-02-02 09:49:52 -0800

    key messed up

--- js/grade.js
+++ js/grade.js
@@ -1,4 +1,4 @@
-for (var i = 0; i <= 00; i++) {
+for (var i = 0; i <= 100; i++) {
     
     switch (true) {
       case (i >= 90):
@@ -21,3 +21,41 @@ for (var i = 0; i <= 00; i++) {
     }
       
 }
+
+
+var grade = 95;
+    switch (true) {
+      case (grade >= 90):
+        console.log("Score " + grade + ' is Grade A');
+        break;
+      case (grade >=80 && grade < 90):
+        console.log("Score " + grade + ' is Grade B');
+        break;
+      case (grade >= 70 && grade < 80):
+        console.log("Score " + grade + ' is Grade C');
+        break;
+      case (grade >= 60 && grade < 70):
+        console.log("Score " + grade + ' is Grade D');
+        break;
+      case (i < 60):
+        console.log("Score " + grade + ' is Grade F');
+        break;
+      default:
+        console.log('Who knows?');
+    } 
+
+//how to stacked case
+    // var letterGrade = 'B';
+    // switch (letterGrade) {
+    //   case 'A':
+    //     console.log(letterGrade, 'is Great');
+    //     break;
+    //   case 'B':
+    //   case 'C':
+    //   case 'C':
+    //     console.log(letterGrade, 'is Fair');
+    //     break;
+    //   case 'D':
+    //     console.log(letterGrade, 'is need to Catch up');
+    //     break;
+    // }
