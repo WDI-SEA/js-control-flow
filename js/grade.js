091ef7711cf33f8a958aa5ef930df0967a57fb58
JRgerv <jack.gervais@gmail.com>
2017-02-02 06:38:38 -0800

    1st commit

---
+++ js/grade.js
@@ -0,0 +1,13 @@
+var score = "61"
+
+if(score>89 && score<100){
+  console.log("A")
+}else if(score>79 && score<90){
+  console.log ("B")
+}else if(score>69 && score<80){
+  console.log ("C")
+}else if(score>59 && score<70){
+  console.log ("D")
+}else if(score>49){
+  console.log ("F")
+}
---
+++ js/pluralizer.js
@@ -0,0 +1,9 @@
+var thing = 'cat';
+var count = '1';
+thing
+
+if(count == 1){
+  console.log('cat')
+}else{
+  console.log("cats")
+}
