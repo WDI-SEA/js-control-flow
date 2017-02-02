fdd61c493a990e980000aac51012cdb21448a39e
JRgerv <jack.gervais@gmail.com>
2017-02-02 09:43:51 -0800

    2nd commit

--- js/grade.js
+++ js/grade.js
@@ -1,13 +1,16 @@
-var score = "61"
+var grade = 61
 
-if(score>89 && score<100){
-  console.log("A")
-}else if(score>79 && score<90){
-  console.log ("B")
-}else if(score>69 && score<80){
-  console.log ("C")
-}else if(score>59 && score<70){
-  console.log ("D")
-}else if(score>49){
-  console.log ("F")
+switch(true){
+  case grade > 90:
+  console.log("A");
+  break;
+case grade > 80:
+  console.log("B");
+  break;
+  case grade > 70:
+  console.log("C");
+break;
+  case grade <70:
+  console.log("Failed");
+  break;
 }
--- js/tempConvert.js
+++ js/tempConvert.js
@@ -1,4 +1,4 @@
 var tempF="45";
-var tempC=(tempF-32)*5/9;
+var tempC=(tempF-32)*(5/9);
 
 console.log(tempF + " degrees fahrenheit is" + tempC+" degrees Celsius.")
