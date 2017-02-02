4939b3e5102d021b53dee64dc173e1b8fce126ba
vogtn <nicolasjvogt@gmail.com>
2017-02-01 16:56:59 -0800

    grade

---
+++ js/grade.js
@@ -0,0 +1,19 @@
+var grade = null;
+console.log("your grade is: ");
+
+switch (true){
+  case(grade > 90):
+    console.log("A");
+    break;
+  case(grade > 80):
+    console.log("B");
+    break;
+  case(grade > 70):
+    console.log("C");
+    break;
+  case(grade > 60):
+    console.log("D");
+    break;
+  case(grade < 59):
+    console.log("F");
+}
