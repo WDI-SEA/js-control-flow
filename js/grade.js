425b890c8c760748f8973f97db0d0f82f02b5a8b
Nicholas Tsafos <nikotsafos@Nicholass-MacBook-Pro.local>
2018-05-23 18:28:25 -0700

    solutions

---
+++ js/grade.js
@@ -0,0 +1,18 @@
+var grade = 93;
+
+switch (true) {
+	case (grade >= 90):
+	console.log('Good Job! You got an A!!');
+	break;
+	case (grade >= 80):
+	console.log('Nice! You got a B!');
+	break;
+	case (grade >= 70):
+	console.log('C');
+	break;
+	case (grade >= 60):
+	console.log('D');
+	break;
+	default:
+	console.log('You failed. better study.');
+}
---
+++ js/tempConvert.js
@@ -0,0 +1,6 @@
+var fer = 68;
+var cel = (fer - 32) * 5/9;
+
+
+console.log(fer + ' ' + 'degrees Fahrenheit is' + ' ' + cel + ' ' + 'degrees Celsius');
+
