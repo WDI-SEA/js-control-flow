6e7fb64dadda47d8799b6744323a88850bc4684d
Eva Barrett <Evabarrett23@Gmail.com>
2018-05-23 22:29:58 -0700

    updated switch statement

--- js/grade.js
+++ js/grade.js
@@ -1,3 +1,31 @@
+
+// assignment grade.js
+var testScore = 88;
+
+switch(true) {
+  case testScore >= 90:
+    console.log('A');
+    break;
+  case testScore >= 80 && testScore <= 89:
+    console.log('B');
+    break;
+  case testScore >= 70 && testScore <= 79:
+    console.log('C');
+    break;
+  case testScore >= 60 && testScore <= 69:
+    console.log('D');
+    break;
+  default:
+    console.log('F');
+    break;
+}
+
+
+
+
+
+
+// practice -before final solution
 var testScore = 85;
 
 switch(true) {
@@ -5,7 +33,7 @@ switch(true) {
     console.log('You got an A, great work!');
     break;
   case testScore >= 80 && testScore <= 89:
-    console.log('You got a B, not to bad!');
+    console.log('You got a B, not too bad!');
     break;
   default:
     console.log("Keep working hard; you'll get there");
