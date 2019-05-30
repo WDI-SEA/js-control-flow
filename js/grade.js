9fc0df110799e3d1597b620983e4a469adf61309
Anthony Riojas <anthonyr@Anthonys-MacBook-Pro.local>
2019-05-30 10:55:42 -0700

    homework submit

---
+++ js/grade.js
@@ -0,0 +1,37 @@
+//var grade = 55;
+
+//switch(true) {
+//	case (90<=grade && grade<=100):
+//		console.log('You got an A!');
+//		break;
+//	case (80<=grade && grade<90):
+//		console.log("You got a B!");
+//		break;
+//	case (70<=grade && grade<80):
+//		console.log('You got a C!');
+//		break;
+//	case (60<=grade && grade<70):
+//		console.log('You got a D!');
+//		break;
+//	case (50<=grade && grade<60):
+//		console.log('You got an F!');
+//        break;
+//    default:
+//        console.log('Try again');
+//       break;
+//   }
+    
+    var grade = 75
+
+    if (grade >= 90 && grade <= 100) {
+        //grade a
+    } else if (grade >= 80) {
+        //grade b
+    } else if (grade >= 70) {
+        // grade c
+    } else {
+        // grade f
+    }
+    // in class solution
+    // used if...else
+    
---
+++ js/pluralizer.js
@@ -0,0 +1,15 @@
+var thing = 'cat';
+var count = 5;
+
+if (count > 1) {
+    console.log(count + '' + thing + 's');
+} else {
+    console.log(count + ''+thing);
+}
+
+// in class solution
+//if (count >1) {
+//    console.log(count.toString(), thing + 's');
+//} else {
+//   console.log("1", thing)
+//}
