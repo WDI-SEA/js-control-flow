30e3d4ea8007edaad7321033452b217fcb13d81b
Garrison28 <ghighsmith28@gmail.com>
2019-09-18 16:34:18 -0700

    done

---
+++ js/translator.js
@@ -0,0 +1,11 @@
+var greeting = 'EN';
+
+if (greeting === 'EN') {
+    console.log('Hello World');
+} else if (greeting === 'JP') {
+    console.log('こんにちは世界');
+} else if (greeting === 'FR') {
+    console.log('Bonjour le monde');
+} else if (greeting === 'GR') {
+    console.log('Hallo Welt');
+};
