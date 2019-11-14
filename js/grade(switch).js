435d21767d1427d172bf06fe600edc4010892d4c
nire9221 <nire9221@gmail.com>
2019-11-13 22:44:38 -0800

    js-control-flow / Jinyoung

---
+++ js/grade(switch).js
@@ -0,0 +1,28 @@
+/*  
+Rewrite your first answer in grade.js using a switch statement!
+*/
+
+var Grade = (Math.random() * 101);
+
+switch (Grade) {
+    case (Grade <= 100 && Grade > 90):
+        console.log("your score is A");
+        break;
+
+    case (Grade <= 90 && Grade > 80):
+        console.log("your score is B");
+        break;
+
+    case (Grade <= 80 && Grade > 70):
+        console.log("your score is C");
+        break;
+
+    case (Grade <= 70 && Grade > 60):
+        console.log("your score is D");
+        break;
+
+    case (Grade <= 60):
+        console.log("your score is F");
+        break;
+
+}
