d8088a7c4a85a27ce996e3924b3f51ec0c3c680e
Durohombredeez7 <Durohombredeez7@gmail.com>
2019-11-13 19:26:45 -0800

    tempupdated

--- js/tempConvert.js
+++ js/tempConvert.js
@@ -1,9 +1,9 @@
 var F = 68;
 var C = (F - 32) * 5/9;
 var K = (F - 32) * 5/9 + 273.15;
-var targetTemp = 85;
+var targetTemp = 'k';
 
-if (targetTemp = C) {
+if (targetTemp === 'c') {
     console.log(F + " degrees Fahrenheit is " + C + " degrees Celsius")
 } else {
     console.log(F + " degrees Fahrenheit is " + K + " degrees Kelvin")
