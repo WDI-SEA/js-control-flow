119473245cff512a4cbeb80ec28a66bde6c2331d
Durohombredeez7 <Durohombredeez7@gmail.com>
2019-11-14 08:45:28 -0800

    translated

--- js/translator.js
+++ js/translator.js
@@ -1,4 +1,17 @@
 var en = 'Hello world';
 var fr = 'Bonjour le monde';
 var es = 'Hola Mundo';
+var de = 'Hallo Welt';
+var chooseLanguage = 'fr';
 
+if (chooseLanguage === 'en') {
+    console.log(en)
+} else if (chooseLanguage === 'fr') {
+    console.log(fr)
+} else if (chooseLanguage === 'es') {
+    console.log(es)
+} else if (chooseLanguage === 'de') {
+    console.log(de)
+} else {
+    console.log('Dont have a translation')
+};
