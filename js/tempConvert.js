f43be9fe47c64e51a947c03f51fa155d3be9e274
bjgoddard <bjgoddard94@gmail.com>
2019-11-14 08:55:21 -0800

    did the homework

---
+++ js/tempConvert.js
@@ -0,0 +1,4 @@
+var tempFahr = 40;
+var tempK = (tempFahr - 32) * (5/9) + 273.15;
+var tempC = (tempFahr - 32) * (5/9);
+console.log(tempFahr + " in Fahrenheit is " + tempK + " in Kelvin, and " + tempC + " in Celsius.")
