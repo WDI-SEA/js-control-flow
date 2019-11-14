1cf32a0b342626ac127349364aa86734fbce648e
Chunrong <56704548+wangchunrong1983@users.noreply.github.com>
2019-11-13 22:02:42 -0800

    turn in assignment

---
+++ js/larger.js
@@ -0,0 +1,6 @@
+var num1=7; var num2=3;
+
+if (num1>num2){
+    console.log(num1 + " " + "is greater than" + " " + num2)
+}
+
---
+++ js/tempConvert.js
@@ -0,0 +1,7 @@
+var Fanraheittemp =70
+var celsiusTemp=((Fanraheittemp-30)/2)*1.8;
+
+console.log ( Fanraheittemp + " " + "degree Fanrenheit is "  + celsiusTemp + " " + "degree Celsius" );
+
+
+
