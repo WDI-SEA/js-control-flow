c1e01a7812adf041b0feba03704dda1df1b576cd
hannahlegros <hannahrlegros@gmail.com>
2021-10-14 17:28:17 -0400

    age.js

---
+++ js/age.js
@@ -0,0 +1,15 @@
+let age = 11
+
+if (age<=1) {
+  console.log('You are an infant')
+} else if (age<=4){
+  console.log('You are a toddler')
+} else if (age<=10) {
+  console.log('You are a child')
+} else if (age<=12) {
+  console.log('You are a preteen')
+} else if (age<=17) {
+  console.log('You are a teen')
+} else if (age<=20) {
+  console.log('You are a young adult')
+}
