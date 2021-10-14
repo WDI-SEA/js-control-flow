30b96831c65ea2666381f591aefd9a658a23ab1f
hannahlegros <hannahrlegros@gmail.com>
2021-10-14 17:11:25 -0400

    age.js

---
+++ js/bonus.js
@@ -0,0 +1,37 @@
+let age = "14"
+
+switch (age) {
+  case "0":
+  case "1":
+    console.log('You are an infant')
+    break;
+  case "2":
+  case "3":
+  case "4":
+    console.log('You are a toddler')
+    break;
+  case "5":
+  case "6":
+  case "7":
+  case "8":
+  case "9":
+  case "10":
+    console.log('You are a child')
+    break;
+  case "11":
+  case "12":
+    console.log('You are a preteen')
+    break;
+  case "13":
+  case "14":
+  case "15":
+  case "16":
+  case "17":
+    console.log('You are a teen')
+    break;
+  case "18":
+  case "19":
+  case "20":  
+    console.log('You are a young adult')
+    break;
+}
