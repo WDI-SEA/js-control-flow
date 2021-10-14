b782fa5a87cde5f658f288d7ffee01c13a43783a
steven43533 <steven43533@gmail.com>
2021-10-13 20:24:20 -0400

    Updated age.js file

--- js/age.js
+++ js/age.js
@@ -1,15 +1,16 @@
-let age = 20;
+let age = Math.floor(Math.random()*20)
+console.log(age)
 switch(true) {
   case (0<=age && age<=1):
     console.log('Infant')
     break;
-  case (2<=age && age<3):
+  case (2<=age && age<=3):
     console.log('Toddler')
     break;
-  case (4<=age && age<12):
+  case (4<=age && age<=12):
     console.log('Child')
     break;
-  case (13<=age && age<16):
+  case (13<=age && age<=16):
     console.log('Preteen')
     break;
   case (17<=age && age<=20):
