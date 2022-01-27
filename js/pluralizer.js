b63eb681e303eb1febbf919dfdefcac833c1c31c
kokopuffz <chanheeya@gmail.com>
2022-01-27 01:57:55 -0600

    update pluralizer.js

--- js/pluralizer.js
+++ js/pluralizer.js
@@ -3,7 +3,7 @@ let count = 2;
 
 function pluralizer(thing, count){
     if (count < 2) {
-        console.log('thing)
+        console.log(count + ' ' +thing)
     } else {
         console.log(count + ' ' + thing + 's')
     }
