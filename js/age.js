08fb9db1d5bc16b8bf07f21497d947e4417bfa35
dmcclintock13 <daenamcclintock@Daenas-MacBook-Air.local>
2022-02-09 21:43:40 -0500

    JS-CONTROL-FLOW-HW

--- js/age.js
+++ js/age.js
@@ -1,1 +1,18 @@
+let age;
+let aged = [];
 
+if (age < 2) {
+    console.log("infant");
+} else if (age < 4) {
+    console.log("toddler");
+} else if (age < 10) {
+    console.log("child");
+} else if (age < 13) {
+    console.log("preteen");
+} else if (age < 20) {
+    console.log("teen");
+} else {
+    console.log("young adult");
+}
+
+aged.push(age);
