cb81c6487cc607320cfb04345ef5072cafed0d69
daenamcclintock <daena.mcclintock@gmail.com>
2022-03-04 16:54:41 -0500

    amended ComparingArrays in js-control-flow

--- js/comparingArrays.js
+++ js/comparingArrays.js
@@ -1,8 +1,31 @@
-let arr1 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
-let arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
+const arr1 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
+const arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
+const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
+const arr4 = [0, 2, 4, 6];
 
 for (let i = 0; i < arr1.length; i++) {
     if (arr1[i] === arr2[i]) {
+    return true;
+    }
+    return false;
+}
+
+for (let i = 0; i < arr1.length; i++) {
+    if (arr2[i] === arr3[i]) {
+    return true;
+    }
+    return false;
+}
+
+for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] === arr4[i]) {
+    return true;
+    }
+    return false;
+}
+
+for (let i = 0; i < arr1.length; i++) {
+    if (arr3[i] === arr4[i]) {
     return true;
     }
     return false;
