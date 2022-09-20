43789d822d1179778db3b6ddc2c86e2deac7fd4e
fjalafoo <fjalafoo@gmail.com>
2022-09-20 20:05:32 +0300

    Solved larger.js

---
+++ js/larger.js
@@ -0,0 +1,21 @@
+//larger.js
+//Write a program that reads two Number variables and prints which one is greater. (e.g. "The larger number of 7 and 3 is 7")
+
+let first = 480
+let second = 240
+
+if(first > second){
+  console.log('The larger between '+first+' and '+second+' is '+first)
+}
+
+else if(first < second ){
+  console.log('The larger between '+first+' and '+second+' is '+second)
+}
+
+else if(first === second){
+  console.log('The larger between '+first+' and '+second+' is BOTH, thought u tricked me human!')
+}
+
+else {
+  console.log('NA-DA')
+}
