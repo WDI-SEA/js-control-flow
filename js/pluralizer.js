b472769cb49bb463d1f385a6ce688fb9ab3fe2a8
fjalafoo <fjalafoo@gmail.com>
2022-09-20 19:38:01 +0300

    Solved pluralizer.js

---
+++ js/pluralizer.js
@@ -0,0 +1,17 @@
+let thing = 'Dalmatian';
+let count = 101; //if you get it, you GET IT!
+
+if(count>=0){
+  if(count===0){
+    console.log('There are '+count+' '+thing+'s')
+  }
+  else if(count===1){
+    console.log('There is '+count+' '+thing)
+  }
+  else{
+    console.log('There are '+count+' '+thing+'s')
+  }
+}
+else {
+  console.log('cannot be a negative number')
+}
