50b346f0af05af81af1835f934cbf6d2a1657271
fjalafoo <fjalafoo@gmail.com>
2022-09-20 20:56:58 +0300

    Solved Translator hopefully right

---
+++ js/translator.js
@@ -0,0 +1,61 @@
+//translate
+//English
+
+
+// let en='HELLO WORLD!'
+
+// //Spanish
+// let es='HOLA MUNDO'
+
+// //Turkish
+// let tr='SELAM DÜNYA'
+
+// //Italian
+// let it='CIAO MONDO'
+
+// //Japanese 
+// let ja ='こんにちは世界'
+
+// //Korean
+// let kr = '헬로월드'
+
+// //German
+// let de = 'HALLO WELT'
+
+// //Russian
+// let ru = 'ПРИВЕТ, МИР'
+
+// //French
+// let fr='BONJOUR LE MONDE'
+
+
+let langcode = 'tr'
+
+if(langcode==='es'){
+  console.log('HELLO WORLD in Spanish (es): HOLA MUNDO')
+}
+else if(langcode==='tr'){
+  console.log('HELLO WORLD in Turkish (tr): SELAM DÜNYA')
+}
+else if(langcode==='it'){
+  console.log('HELLO WORLD in Italian (it): CIAO MONDO')
+}
+else if(langcode==='ja'){
+  console.log('HELLO WORLD in Japanese (ja): こんにちは世界')
+}
+else if(langcode==='kr'){
+  console.log('HELLO WORLD in Korean (kr): 헬로월드')
+}
+else if(langcode==='de'){
+  console.log('HELLO WORLD in German (de): HALLO WELT')
+}
+else if(langcode==='ru'){
+  console.log('HELLO WORLD in Russian (ru): ПРИВЕТ, МИР')
+}
+else if(langcode==='fr'){
+  console.log('HELLO WORLD in French (fr): BONJOUR LE MONDE')
+}
+
+else {
+  console.log('HELLO WORLD!')
+}
