ec1d5751421e7005b8e773982a636f6606f83bc2
Anwaar <anwaaralsyouf@gmail.com>
2022-09-20 22:43:40 +0300

    translator program added

---
+++ js/translator.js
@@ -0,0 +1,23 @@
+//choose  a language: kr jp thai ar fr en
+let language = "jp"
+
+let korean = "헬로월드", japanese = "こんにちは世界", arabic = "مرحبا بالعالم",  french = "Bonjour le monde",  thailand ="สวัสดีชาวโลก", english = "Hello World"
+
+if(language == "en"){
+  console.log(english)
+}
+else if(language == "ar"){
+  console.log(arabic)
+}
+else if(language == "fr"){
+  console.log(french)
+}
+else if(language == "kr"){
+  console.log(korean)
+}
+else if(language == "jp"){
+  console.log(japanese)
+}
+else if(language == "thai"){
+  console.log(thailand)
+}
