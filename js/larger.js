e3978c7d2dd6f6c5b41d3fd7073d028250763b43
Anwaar <anwaaralsyouf@gmail.com>
2022-09-20 22:25:21 +0300

    larger program added

---
+++ js/larger.js
@@ -0,0 +1,12 @@
+let firstNum = 17
+let secondNum = 4
+
+if(firstNum === secondNum){
+  console.log("The numbers you entred " + firstNum + " and " + secondNum + " are equal!")
+}
+else if(firstNum > secondNum){
+  console.log("The larger number of " + firstNum + " and " + secondNum + " is " + firstNum)
+}
+else{
+  console.log("The larger number of " + firstNum + " and " + secondNum + " is " + secondNum)
+}
