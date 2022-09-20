57c5a64b85a072f47ad490554c2196dbcb0a3e27
Anwaar <anwaaralsyouf@gmail.com>
2022-09-20 21:27:28 +0300

    age program added

--- js/age.js
+++ js/age.js
@@ -1,1 +1,22 @@
+//variable age
+let age = 0
 
+// if else statemnets based on the age
+if (age >= 0 && age <= 1) {
+  console.log("You're an Infant what r u doing here? 🤨")
+}
+else if (age > 1 && age <= 4) {
+  console.log("You're a Todler!")
+}
+else if (age > 5 && age <= 10) {
+  console.log("You're a Child!")
+}
+else if (age > 10 && age <= 13) {
+  console.log("You're a Preteen!")
+}
+else if (age > 13 && age <= 17) {
+  console.log("You're a Teen!")
+}
+else if (age > 17 && age <= 20) {
+  console.log("You're a young Adult!")
+}
