0383ab6f52eb10219dc317c21998ac187de349b2
FatimaAliAbdulla6248 <105248089+FatimaAliAbdulla6248@users.noreply.github.com>
2022-09-20 21:17:40 +0300

    Update age.js

--- js/age.js
+++ js/age.js
@@ -1,1 +1,26 @@
+ var ages =[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20];
 
+let age = "3";
+switch(true) {
+  case (1>age):
+    console.log('Infant');
+    break;
+  case (1<=age && age<=3):
+    console.log('Toddler');
+    break;
+  case ( 4<=age && age<=8):
+    console.log('Child');
+    break;
+  case ( 9<=age && age<=13):
+    console.log('Pre-teen');
+    break;
+  case ( 13<=age && age<=17):
+    console.log('Teen');
+    break;
+  case ( 18<=age && age<=20):
+    console.log('Young Adult');
+    break;
+  default:
+    console.log('Try again next time!');
+    break;
+}
