8c9f7723d58a33c11739c52701c87006a92fbac1
“Mohamed-salman” <sqweezy.m@gmail.com>
2022-09-21 08:58:42 +0300

    done

--- js/age.js
+++ js/age.js
@@ -1,1 +1,27 @@
+let age = -4
 
+if (age < 0) {
+  console.log ("please enter a correct age")
+}
+
+else if (age < 3) {
+  console.log ("infant")
+}
+else if (age < 7) {
+  console.log ("toddler")
+}
+else if (age < 10) {
+  console.log ("child")
+}
+else if (age < 14) {
+  console.log ("preteen")
+}
+else if (age < 17) {
+  console.log ("teen")
+}
+else if (age < 21) {
+  console.log ("young adult")
+}
+else (age > 21) {
+    console.log ("the age is not accepted")
+}
---
+++ js/larger.js
@@ -0,0 +1,8 @@
+let number = [13, 8]
+
+if (13 > 8) {
+  console.log ("13")
+}
+else (13 < 8) {
+  console.log ("13 is less than 8")
+}
