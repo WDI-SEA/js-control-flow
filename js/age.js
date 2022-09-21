99c212df13ede4bec0066b1052dc932160c490d6
Fuad AlShuroogi <fmjs@Fuads-MacBook-Air.local>
2022-09-21 10:07:29 +0300

    Fuad AlShuroogi - Age program done ..

--- js/age.js
+++ js/age.js
@@ -1,1 +1,19 @@
+let age = prompt("How old are you?")
 
+switch(true) {
+	case (age<=1):
+		console.log("infant")
+		break
+	case (age>1 && age<=5):
+		console.log("toddler")
+	case (age>5 && age<=9):
+		console.log("child")
+	case (age>9 && age<=12):
+		console.log("preeteen")
+	case (age>12 && age<=17):
+		console.log("teen")
+	case (age>17 && age<=20):
+		console.log("Young adult")
+	default:
+		console.log("invalid input, please enter an integer between 0 and 20 ")
+}
