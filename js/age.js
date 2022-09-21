d5cc94339b7f33bf59b09dc11718a6037a947c85
NoorAlaaraj <Noor_alaaraj@outlook.com>
2022-09-21 08:59:02 +0300

    Only Age

--- js/age.js
+++ js/age.js
@@ -1,1 +1,26 @@
+let age = 15
+
+if (age == 0) {
+  console.log('infant')
+}
+
+else if (age <= 1) {
+  console.log('toddler')
+}
+
+else if (age <= 3) {
+  console.log('child')
+}
+
+else if (age <= 13) {
+  console.log('preteen')
+}
+
+else if (age <= 18) {
+  console.log('teen')
+}
+
+else if (age <= 20) {
+  console.log('young adult')
+}
 
