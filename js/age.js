067985369316a25977a647b8ac730158a4bb1bcb
NoorAlaaraj <Noor_alaaraj@outlook.com>
2022-09-21 10:08:42 +0300

    two and three

--- js/age.js
+++ js/age.js
@@ -23,4 +23,26 @@ else if (age <= 18) {
 else if (age <= 20) {
   console.log('young adult')
 }
+-------------------
 
+let thing = ' cat'
+let count = ' 1'
+
+if (count == 1) { console.log(count, thing) }
+
+
+else
+  console.log(count, thing + 's')
+
+------------------
+let temp = '100'
+let degree = 'F'
+
+console.log(temp, degree)
+
+convert = (temp - 32) * 5 / 9
+let Degree = 'C'
+
+console.log(convert, Degree)
+
+-------------------
