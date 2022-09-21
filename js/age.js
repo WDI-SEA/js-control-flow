c1f8dc38696c9a3d0b0092ba75bd97ec8bed4188
Fuad AlShuroogi <fmjs@Fuads-MacBook-Air.local>
2022-09-21 10:46:44 +0300

    Fuad AlShuroogi - Updated the Age program and added comparing arrays file to start working on it

--- js/age.js
+++ js/age.js
@@ -1,4 +1,6 @@
-let age = prompt("How old are you?")
+let age = parseInt(prompt("How old are you?"));
+
+//console.log(typeof age);
 
 switch(true) {
 	case (age<=1):
@@ -6,14 +8,19 @@ switch(true) {
 		break
 	case (age>1 && age<=5):
 		console.log("toddler")
+    break
 	case (age>5 && age<=9):
 		console.log("child")
+    break
 	case (age>9 && age<=12):
 		console.log("preeteen")
+    break
 	case (age>12 && age<=17):
 		console.log("teen")
+    break
 	case (age>17 && age<=20):
 		console.log("Young adult")
+    break
 	default:
 		console.log("invalid input, please enter an integer between 0 and 20 ")
 }
---
+++ js/comparing arrays.js
@@ -0,0 +1,4 @@
+let a = [1,2,3];
+let b = [1,2,3];
+
+console.log(a === b);  // This will never be true
