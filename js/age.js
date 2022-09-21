99f0a28dcd9dfe839be2e0be02ecf029c97d67d8
Sabika Ali <Sabikaalghadeeri@gmail.com>
2022-09-21 06:09:17 +0300

    files of my answers

--- js/age.js
+++ js/age.js
@@ -1,1 +1,18 @@
-
+var age = window.prompt("Enter Your Age:")
+if (age >=0 && age <= 1) {
+   window.alert("You are an infant")
+  
+} else if (age >= 2 && age <= 5) {
+   window.alert("You are a toodler")
+  
+ } else if (age >= 6 && age <= 9){
+   window.alert("You are a chiled")
+  
+ } else if (age >= 10 && age <= 12) {
+   window.alert("You are a preteen")
+   
+ } else if (age >= 13 && age <= 19) {
+   window.alert("You are a teen")
+   
+ } else (age = 20)
+   window.alert("You are a young adult")
