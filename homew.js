9b7ee7832b2bc8607c495c86b95f3c3a0445d4c8
MramRashidR <mramrashid98@gmail.com>
2022-09-21 08:31:21 +0300

    ex 1 modif

--- homew.js
+++ homew.js
@@ -1,18 +1,20 @@
 //ex 1
 
-//let age = 20
+//let age = 12
 
-if (age == 0) {
+if (age == 0 ) {
     console.log('infant') }
-  else if (age >=3) {
+  else if (age <=3) {
    console.log('toddler')
-  } else if (age >=8) {
+  } else if (age <=8) {
     console.log('Child')
-  } else if (age >=12) {
+  } else if (age <=12) {
     console.log('preteen')
-  } else if (age>=17) {
+  } else if (age<=17) {
     console.log('teen') }
-    else if (age>=20 ) {
+    else if (age<=20 ) {
     console.log('young adult')
-  }  
+  } 
   
+
+  //ex2
