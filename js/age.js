218a4d69655e2cad0dcdd5afd95f387d4e5886c2
Nathanael Johnson <nathanealjohnson@gmail.com>
2022-09-21 09:51:29 +0300

    Created age

--- js/age.js
+++ js/age.js
@@ -1,1 +1,21 @@
 
+var age = prompt("How old are you?");
+
+if(age<1)
+{
+  console.log("infant")
+}else if(age >=1 && age<=5){
+console.log("toddler")
+}
+else if(age >=5 && age<=9){
+console.log("child")
+}
+else if(age >=9 && age<12){
+console.log("preteen")
+}
+else if(age >=12 && age<=17){
+console.log("teeen")
+}
+else if(age >=17 && age<=20){
+console.log("young adult")
+}
