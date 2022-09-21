5020454b5618f745291fc96dd1a118af891a77d3
Ahmed Al-Thawadi <ahmed9althawadi@gmail.com>
2022-09-21 10:28:02 +0300

    Modified compared array!

--- js/comparingArrays.js
+++ js/comparingArrays.js
@@ -15,6 +15,9 @@ const arr2 = [1, 'a', 3, 'e', 5, 7, 9, 'i'];
 const arr3 = [1, 'a', 3, 5, 'e', 7, 10, 'i'];
 const arr4 = [0, 2, 4, 6];
 
+
+
+//First Method
 if(arr2.length !== arr3.length){
     console.log("The two array are not equal!")
 } else {
@@ -34,3 +37,10 @@ if(arr2.length !== arr3.length){
     }
 }
 
+
+//Second method
+if(arr1.join()===arr3.join()){
+    console.log("They are equal")
+  } else {
+    console.log("They are not equal")
+  }
