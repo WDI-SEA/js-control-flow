5ca7e7a6375cd93f2ee8537ad38d04bc36b3d400
Sharifaaz <Sharifaalzayed96@gmail.com>
2022-09-21 15:32:05 +0300

    Completed the age Part only

--- js/age.js
+++ js/age.js
@@ -1,1 +1,31 @@
-
+function ageJS(age) {
+ 
+    var=age;
+  
+  if (age===0 || age===1) {
+    
+    console.log('infant');
+    
+    else if (age > 1 && age <6)
+    
+    console.log('toddler);g
+                
+    else if (age > 5 && age < 13)
+      
+      console.log('child');
+      
+    else if(age > 12 && age < 17)
+  
+      console.log('preteen');
+    
+    else if(age > 16 && age < 20) 
+  
+      console.log('teen');
+  
+    else if(age === 20)
+      
+      console.log('young adult');
+    
+  }
+  }
+  ageJS(20)
