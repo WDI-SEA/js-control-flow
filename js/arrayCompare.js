d14ac15db2c3691a73a7cee2cf0ae44c79739416
Ameera Ali <amiraabdullamir@gmail.com>
2022-09-21 10:33:33 +0300

    Added arrayCompare.js

---
+++ js/arrayCompare.js
@@ -0,0 +1,20 @@
+let a =[1,2,"3"]
+let b = [1,2,3,]
+if(a.length == b.length){
+    //The arrays are the same length
+    for (let i =0;i<a.length;i++){
+    if(a[i]!==b[i]){
+       console.log("The arrays aren't the same")
+       break;
+        if(a[i]=a[a.length-1]){
+            console.log("the arrays are the same ")
+        }
+    }
+
+}   
+}  
+ else{
+        console.log("The arrays are not the same length")
+}
+
+ 
