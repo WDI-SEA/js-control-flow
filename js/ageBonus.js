b26475bab6a33148ca3303ddb4294bdfe1149b2e
fjalafoo <fjalafoo@gmail.com>
2022-09-21 09:40:02 +0300

    modified Bonus

--- js/ageBonus.js
+++ js/ageBonus.js
@@ -4,37 +4,22 @@ let age_category = ["infant", "toddler", "child", "preteen", "teen", "young adul
 
 let age = 120
 
-switch (age) {
-  case 0:
-  case 1:
+switch (true) {
+  case (age<=1):
     console.log(age_category[0])
     break
-  case 2:
-  case 3:
+  case (age<=3):
    console.log(age_category[1])
    break
-  case 4:
-  case 5:
+  case (age<=5):
     console.log(age_category[2])
-  case 6:
-  case 7:
-  case 8:
-  case 9:
-  case 10:
-  case 11:
-  case 12:
+  case (age<=12):
      console.log(age_category[3])
     break
-  case 13:
-  case 14:
-  case 15:
-  case 16:
-  case 17:
-  case 18:
+  case (age<=18):
      console.log(age_category[4])
     break
-  case 19:
-  case 20:
+  case (age<=20):
      console.log(age_category[5])
     
   default:
