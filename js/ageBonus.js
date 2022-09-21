b5306b1e8a6389a0bf1fa10a3aa6f5f750889cf9
fjalafoo <fjalafoo@gmail.com>
2022-09-21 09:56:59 +0300

    last modification on bonus

--- js/ageBonus.js
+++ js/ageBonus.js
@@ -1,25 +1,26 @@
+//hw
 //we have 6 categories, we can store in an array.
 let age_category = ["infant", "toddler", "child", "preteen", "teen", "young adult"]
 
 
-let age = 120
+let age = 12
 
 switch (true) {
   case (age<=1):
     console.log(age_category[0])
     break
-  case (age<=3):
+  case (age>2 && age <=4):
    console.log(age_category[1])
    break
-  case (age<=5):
+  case (age>4 && age <=6):
     console.log(age_category[2])
-  case (age<=12):
+  case (age > 7 && age<=12 ):
      console.log(age_category[3])
     break
-  case (age<=18):
+  case (age>12 && age<=17):
      console.log(age_category[4])
     break
-  case (age<=20):
+  case (age > 17 && age<=20):
      console.log(age_category[5])
     
   default:
