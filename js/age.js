405dc849fc2903bf202b2f401b58b2b6045e1c6a
s20 <salsaleh09@mail.com>
2022-09-21 12:59:41 +0300

    age problem completed

--- js/age.js
+++ js/age.js
@@ -1,1 +1,8 @@
-
+let age = 10
+if (age<=2) {console.log('infant')}
+else if (age>2 && age<=6) {console.log('child')}
+else if (age>6 && age<=9) {console.log('toddler')}
+else if (age>10 && age<=12) {console.log('preteen')}
+else if (age>=13 && age<=19) {console.log('teen')}
+else {console.log('young adult')}
+//hello 
