0bc6983d5f17251017f8fae839e797bc5693de02
MramRashidR <mramrashid98@gmail.com>
2022-09-21 09:41:58 +0300

    ex 4

---
+++ js/larger.js
@@ -0,0 +1,11 @@
+num1 = 10
+num2 = 7
+
+if (num1 > num2) {
+  console.log(num1 , 'is greater')
+}
+else if (num2 > num1) {
+  console.log(num2 , 'is greater')
+} else {
+  console.log('equal amounts')
+}
